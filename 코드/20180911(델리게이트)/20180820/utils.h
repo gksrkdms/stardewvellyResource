c3703e23e03d1264af9761a51d989703affba32f
gksrkdms <43227811+gksrkdms@users.noreply.github.com>
2018-09-17 00:42:54 +0900

    한가은
    변수명명법과 적이미지 추가

--- 코드/20180911(델리게이트)/20180820/utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace MY_UTIL
-{
-	float getDistance(float startX, float startY, float endX, float endY);
-	float getAngle(float startX, float startY, float endX, float endY);
-}
-
